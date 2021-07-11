0a3e6c96ddec29863971606c1db445bb274ee581
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-07-10 19:45:48 -0700

    Added mobile version of navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown} from 'react-bootstrap';
 import '../styling/navbar.scss';
 import '../styling/globals.scss';
+
+import {ReactComponent as List} from '../svg/list.svg';
+
 export const Nav = () => {
     return(
         <nav className="header-container">
-            <div><p>Logo</p></div>
-            <div className="navigation">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Pricing</li>
-                </ul>
+            <div className="header-container-site">
+                <div><p>Logo</p></div>
+                <div className="navigation">
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Pricing</li>
+                    </ul>
+                </div>
+                <div>
+                    <p><Button variant="light">More Info</Button></p>
+                </div>
             </div>
-            <div>
-                <p><Button variant="light">More Info</Button></p>
+            <div className="header-container-mobile">
+                <div>
+                    <h1>Logo</h1>
+                </div>
+
+                <div>
+                <Dropdown>
+                    <Dropdown.Toggle variant="light" id="dropdown-basic">
+                        <List/>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Home</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Stories</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Features</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                </div>
             </div>
         </nav>
     );
