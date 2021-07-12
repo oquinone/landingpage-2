7e5f994bd4958a0026d7d6cd4f285fdb05267434
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-07-11 21:41:05 -0700

    Added hover effect to show text of image

--- src/components/section_4.js
+++ src/components/section_4.js
@@ -9,10 +9,64 @@ import '../styling/section_4.scss';
 export const Section4 = () => {
     return(
         <section className="container4">
-            <img src={mountain} alt={mountain} loading="lazy"/>
-            <img src={buildings} alt={buildings} loading="lazy"/>
-            <img src={voyage} alt={voyage} loading="lazy"/>
-            <img src={arch} alt={arch} loading="lazy"/>
+            <div className="container4-image">
+                <img 
+                    src={mountain} 
+                    alt={mountain} 
+                    loading="lazy"
+                    className="container4-hover"/>
+                <div className="container4-info">
+                    <div className="text">
+                        <h3>The Mountains</h3>
+                        <small>By John Smith</small>
+                        <h4>READ STORY</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container4-image">
+                <img 
+                    src={buildings} 
+                    alt={buildings} 
+                    loading="lazy"
+                    />
+                <div className="container4-info">
+                    <div className="text">
+                        <h3>Cityscapes</h3>
+                        <small>By Frank Ocean</small>
+                        <h4>READ STORY</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container4-image">
+                <img 
+                    src={voyage} 
+                    alt={voyage} 
+                    loading="lazy"
+                    />
+                <div className="container4-info">
+                    <div className="text">
+                        <h3>22 Day Voyage</h3>
+                        <small>By Alexander Gordon</small>
+                        <h4>READ STORY</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="container4-image">
+                <img 
+                    src={arch} 
+                    alt={arch} 
+                    loading="lazy"
+                    />
+                <div className="container4-info">
+                    <div className="text">
+                        <h3>Architecture</h3>
+                        <small>By Sam Smith</small>
+                        <h4>READ STORY</h4>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
